dbc163312e4d3b94b41590dc5954f829d576d6a7
mishok373 <108998580+mishok373@users.noreply.github.com>
2023-09-19 22:22:58 +0300

    НАКОНЕЦ ТО БЛЯТЬ РАБОТАЕТ!

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import logging
-import  asyncio
+import asyncio
 import g4f
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
-
+import nest_asyncio
+nest_asyncio.apply()
 TOKEN = '6122222610:AAFcGasSkJLYdjldRzv8Rkd6O-IStwHAFrs'
 
 # Включаем логирование, чтобы не пропустить важные сообщения
@@ -14,9 +15,21 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 
+def crtmsg(args):
+    response = g4f.ChatCompletion.create(
+        model=g4f.models.gpt_4,
+        messages=[{"role": "user", "content": args}],
+        provider=g4f.Provider.DeepAi)
+    return response
+
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     await message.answer("Hello!")
+
+@dp.message(Command("test1"))
+async def send(message : types.Message, command):
+    await message.reply(crtmsg(command.args))
+
 
 
 async def main():
