04e90ec570ec395f87c84bfe64ca75243a665152
mishok373 <108998580+mishok373@users.noreply.github.com>
2023-09-20 22:31:07 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import logging
 import asyncio
+import logging
 import g4f
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command
 import nest_asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram import Router
+from aiogram.filters import Command
+
 nest_asyncio.apply()
 TOKEN = '6122222610:AAFcGasSkJLYdjldRzv8Rkd6O-IStwHAFrs'
 
@@ -14,32 +16,35 @@ bot = Bot(token=TOKEN)
 
 dp = Dispatcher()
 
-
-def crtmsg(args):
-    response = g4f.ChatCompletion.create(
+loop = asyncio.new_event_loop()
+async def crtmsg(args):
+    provider = g4f.Provider.DeepAi
+    response = provider.create_async(
         model=g4f.models.gpt_4,
-        messages=[{"role": "user", "content": args}],
-        provider=g4f.Provider.DeepAi)
+        messages=[{"role": "user", "content": args}])
     return response
+
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Hello!")
+    await message.answer("Разраб даун, так что если чтото (не)работает(не так) пишите этому аутисту: @mishok_373\nДоступные команды:\n/start(выводит это говно)\n/t *ваш_запрос*  (основная команда, создающая запрос)")
 
-@dp.message(Command("test1"))
-async def send(message : types.Message, command):
-    await message.reply(crtmsg(command.args))
+@dp.message(Command("t"))
+async def cmd_t(message : types.Message, command):
+    if command.args is not None:
+        await message.reply('Думаю...')
+        await message.reply(str(await asyncio.run(crtmsg(command.args))))
+    else:
+        await message.reply('!вы не вели запрос, либо он слишком короток')
 
 
+# router = Router()
+# @router.message()
+# async def aanswer(message : types.message):
+#     await message.reply('Думаю...')
+#     await message.reply(str(await asyncio.run(crtmsg(message))))
 
 async def main():
     await dp.start_polling(bot)
 
 asyncio.run(main())
-
-# prompt = input()
-# response = g4f.ChatCompletion.create(
-#     model=g4f.models.gpt_4,
-#     messages=[{"role": "user", "content": prompt}],
-#     provider=g4f.Provider.DeepAi)
-# print(response)
